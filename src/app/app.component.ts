8ab6db252b435fa75b8fe3bda0653fea992e0bd7
vasanthanathanQa <vasanthnathan5@gmail.com>
2026-06-29 12:30:36 +0530

    Created few components to understand Angular standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItemComponent } from './menu/menu-item/menu-item.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
