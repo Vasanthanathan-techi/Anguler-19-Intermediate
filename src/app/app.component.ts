e023eebe9149b3a8d19ebdc713a302cfbe17474c
vasanthanathanQa <vasanthnathan5@gmail.com>
2026-07-02 02:10:26 +0530

    add-admin-dashboard-skeleton

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MenuItemComponent } from './menu/menu-item/menu-item.component';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MenuItemComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'app';
+}
