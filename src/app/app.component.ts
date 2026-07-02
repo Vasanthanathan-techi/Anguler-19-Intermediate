7b88e2bce0b1bfa6b64bb23b7143c2bc18efbb37
vasanthanathanQa <vasanthnathan5@gmail.com>
2026-07-02 23:37:42 +0530

    Two-Way Binding [(ngModel)]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideBarComponent } from '../shared/side-bar/side-bar.component';
+import { FormsModule } from '@angular/forms';
+
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,SideBarComponent],
+  imports: [RouterOutlet,SideBarComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 classname='p-5 text-6xl';
 name='vasanthanathan';
-onChange($e: Event){
-   this.name=($e.target as HTMLInputElement).value;
-}
+
 
   }
 
