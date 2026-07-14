44975e07f83d7618c25f97be57bfe4da03bf9b04
vasanthanathanQa <vasanthnathan5@gmail.com>
2026-07-15 04:49:52 +0530

    anguler routing-wild card-parameter-child

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotesComponent } from './notes/notes.component';
+import { TrashComponent } from './trash/trash.component';
+import { InvalidComponent } from './invalid/invalid.component';
+import { NoteComponent } from './notes/note/note.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+
+     { path:'notes', component:NotesComponent,
+        children:[
+            {path:':name',component:NoteComponent}
+     ],
+    },
+      {path:'trash',component:TrashComponent},
+      {path:'**',component:InvalidComponent},
+      {path:'test',redirectTo:'notes',  pathMatch:'full'},
+];
---
+++ src/app/notes/notes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: 'app-notes',
+  imports: [RouterOutlet],
+  templateUrl: './notes.component.html',
+  styleUrl: './notes.component.css'
+})
+export class NotesComponent {
+
+}
