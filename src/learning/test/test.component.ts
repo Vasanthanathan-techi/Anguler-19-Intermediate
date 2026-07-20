a169f124d37314707267d2731bd3f830f26a1ea7
vasanthanathanQa <vasanthnathan5@gmail.com>
2026-07-20 15:40:50 +0530

    signal-computed-effect-injectorinterface-untracked-implement

--- src/learning/test/test.component.ts
+++ src/learning/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,computed,signal,effect,Injector, inject , untracked} from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -6,21 +6,80 @@ import { Component } from '@angular/core';
   templateUrl: './test.component.html',
   styleUrl: './test.component.css'
 })
-export class TestComponent {
-  family =[
-     {name:'nathan vasanth',age:30},
-      {name:'jasmine',age:18},
-        {name:'sam',age:6},
-        {name:'megha',age:2},
-  ];
+export class TestComponent  implements OnInit{
+  injector =inject(Injector)
+name =signal('vasanth nathan');
+age=signal(30);
+gender=signal('female');
+ 
 
-  type: 'KIDS' |'ELDERS'='ELDERS';
-
-  getkids(){
-    return this.family.filter(x=>x.age<13);
+isMajor =computed(()=>{
+  if (this.gender()=='male') {
+    return this.age()>=18?true:false;
+  }else{
+    return false;
   }
-  getelder(){
-    return this.family.filter(X=>X.age>=13);
+  })
+afterEffect = effect(()=>{
+  console.log('the age is'+this.age()+'and my gender is'+ untracked(this.gender))
+  },{injector:this.injector});
+
+  constructor(){
+    
   }
 
+//   constructor(){
+//     effect(()=>{
+// console.log('the age is'+this.age())
+//     });
+//   }
+
+  runAfterEffect(){
+       effect(()=>{
+console.log('the age is'+this.age())
+    },{injector:this.injector});
+  }
+
+ ngOnInit():void{
+  
+//this.name.set('vazzy');
+ // this.name.update((value)=>value+'jasmine')
+
+setTimeout(()=>{
+  this.gender.set('male')
+  //this.age.set(10)
+},2000);
+}
+
+
+
+
+
+ 
+
+
+
+
+
+
+  
+  
+  
+
+  // family =[
+  //    {name:'nathan vasanth',age:30},
+  //     {name:'jasmine',age:18},
+  //       {name:'sam',age:6},
+  //       {name:'megha',age:2},
+  // ];
+
+  // type: 'KIDS' |'ELDERS'='ELDERS';
+
+  // getkids(){
+  //   return this.family.filter(x=>x.age<13);
+  // }
+  // getelder(){
+  //   return this.family.filter(X=>X.age>=13);
+  // }
+ 
 }
